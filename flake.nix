41ffb5e6e0e8e0439c7f805a89069baa2d7761e4
stefan <stefan@noreply.git.chobble.com>
2025-04-01 18:26:49 +0000

    Wrap attributes

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
           buildPhase = ''
             sass --update src/_scss:_site/css --style expanded
             yarn --offline eleventy
-            find _site -name "*.html" -exec tidy --wrap 80 --indent auto --indent-spaces 2  --wrap 80 --quiet yes --tidy-mark no --drop-empty-elements no -modify {} \;
+            find _site -name "*.html" -exec tidy --wrap 80 --indent auto --indent-spaces 2  --wrap 80 --quiet yes --tidy-mark no --drop-empty-elements no --wrap-attributes yes -modify {} \;
           '';
 
           installPhase = ''cp -r _site $out'';
