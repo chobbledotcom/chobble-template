86f982c0f33889e26d4ef565ca624109f8d0bdbe
Stefan <git@stefn.co.uk>
2025-04-02 22:05:43 +0100

    Remove flake-utils

--- flake.nix
+++ flake.nix
@@ -1,89 +1,120 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b?narHash=sha256-l0KFg5HjrsfsO/JpG%2Br7fRrqm12kzFHyUHqHCVpMMbI%3D";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        nodeDeps = import ./node-deps.nix { inherit pkgs; };
-        inherit (nodeDeps) packageJSON nodeModules;
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-        deps = with pkgs; [
-          html-tidy
-          sass
-          yarn
-        ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
 
-        mkScript =
-          name:
-          let
-            base = pkgs.writeScriptBin name (builtins.readFile ./bin/${name});
-            patched = base.overrideAttrs (old: {
-              buildCommand = "${old.buildCommand}\n patchShebangs $out";
-            });
-          in
-          pkgs.symlinkJoin {
-            inherit name;
-            paths = [ patched ] ++ deps;
-            buildInputs = [ pkgs.makeWrapper ];
-            postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+      mkUtils =
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          nodeDeps = import ./node-deps.nix { inherit pkgs; };
+          inherit (nodeDeps) packageJSON nodeModules;
+
+          deps = with pkgs; [
+            html-tidy
+            sass
+            yarn
+          ];
+
+          mkScript =
+            name:
+            let
+              base = pkgs.writeScriptBin name (builtins.readFile ./bin/${name});
+              patched = base.overrideAttrs (old: {
+                buildCommand = "${old.buildCommand}\n patchShebangs $out";
+              });
+            in
+            pkgs.symlinkJoin {
+              inherit name;
+              paths = [ patched ] ++ deps;
+              buildInputs = [ pkgs.makeWrapper ];
+              postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+            };
+
+          scripts = [
+            "build"
+            "serve"
+            "dryrun"
+            "tidy_html"
+          ];
+
+          scriptPkgs = builtins.listToAttrs (
+            map (name: {
+              inherit name;
+              value = mkScript name;
+            }) scripts
+          );
+
+          site = pkgs.stdenv.mkDerivation {
+            name = "eleventy-site";
+            src = ./.;
+            buildInputs = deps ++ [ nodeModules ];
+
+            configurePhase = ''
+              ln -sf ${packageJSON} package.json
+              ln -sf ${nodeModules}/node_modules .
+            '';
+
+            buildPhase = ''
+              ${mkScript "build"}/bin/build
+              ${mkScript "tidy_html"}/bin/tidy_html
+            '';
+
+            installPhase = ''
+              cp -r _site $out
+            '';
+
+            dontFixup = true;
           };
-
-        scripts = [
-          "build"
-          "serve"
-          "dryrun"
-          "tidy_html"
-        ];
-
-        scriptPkgs = builtins.listToAttrs (
-          map (name: {
-            inherit name;
-            value = mkScript name;
-          }) scripts
-        );
-
-        site = pkgs.stdenv.mkDerivation {
-          name = "eleventy-site";
-          src = ./.;
-          buildInputs = deps ++ [ nodeModules ];
-
-          configurePhase = ''
-            ln -sf ${packageJSON} package.json
-            ln -sf ${nodeModules}/node_modules .
-          '';
-
-          buildPhase = ''
-            ${mkScript "build"}/bin/build
-            ${mkScript "tidy_html"}/bin/tidy_html
-          '';
-
-          installPhase = ''
-            cp -r _site $out
-          '';
-
-          dontFixup = true;
+        in
+        {
+          inherit
+            pkgs
+            deps
+            mkScript
+            scripts
+            scriptPkgs
+            site
+            ;
+          inherit packageJSON nodeModules;
         };
-      in
-      rec {
-        defaultPackage = packages.site;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          u = mkUtils system;
+          inherit (u) scriptPkgs site;
+        in
+        scriptPkgs // { inherit site; }
+      );
 
-        packages = scriptPkgs // {
-          inherit site;
-        };
+      defaultPackage = forAllSystems (system: self.packages.${system}.site);
 
-        devShells = rec {
+      devShells = forAllSystems (
+        system:
+        let
+          u = mkUtils system;
+          inherit (u)
+            pkgs
+            deps
+            scriptPkgs
+            packageJSON
+            nodeModules
+            ;
+        in
+        rec {
           default = dev;
-
           dev = pkgs.mkShell {
             buildInputs = deps ++ (builtins.attrValues scriptPkgs);
 
@@ -102,7 +133,7 @@
               git pull
             '';
           };
-        };
-      }
-    );
+        }
+      );
+    };
 }
