cd3629a698c9d6f9a838f01ac8e573dec124aaa5
stefan <stefan@noreply.git.chobble.com>
2025-04-10 17:23:25 +0000

    revert a2d8e08ff027f20a1d7cd5e9b318794fb06b9323
    revert Add default category images

--- src/categories/categories.11tydata.js
+++ src/categories/categories.11tydata.js
@@ -3,13 +3,6 @@ module.exports = {
     header_text: (data) => data.header_text || data.title,
     header_image: (data) => data.header_image || data.featured_image,
     meta_title: (data) => data.meta_title || data.title,
-    featured_image: (data, collections) => {
-      if (data.featured_image) return data.featured_image;
-      const prod = collections.product?.find((p) =>
-        p.data.categories?.includes(data.page.fileSlug),
-      );
-      return prod?.data.header_image || prod?.data.featured_image;
-    },
     eleventyNavigation: (data) => {
       if (data.parent != null) return false;
 
