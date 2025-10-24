e0693650aa396923878d1d13b1fae14234b1ac12
Stefan <git@stefn.co.uk>
2025-10-24 20:24:08 +0100

    Linting

--- src/_lib/prettier.js
+++ src/_lib/prettier.js
@@ -1,28 +1,25 @@
-import prettier from 'prettier';
+import prettier from "prettier";
 
 function createPrettierTransform(options = {}) {
-  return async function prettierTransform(content, outputPath) {
-    if (outputPath && !outputPath.endsWith('.html')) {
-      return content;
-    }
-    
-    // Skip prettier for feeds - they don't need HTML formatting
-    if (outputPath && outputPath.includes('/feed.')) {
-      return content;
-    }
-    
-    return await prettier.format(content, {
-      parser: 'html',
-      ...options
-    });
-  };
+	return async function prettierTransform(content, outputPath) {
+		if (outputPath && !outputPath.endsWith(".html")) {
+			return content;
+		}
+
+		// Skip prettier for feeds - they don't need HTML formatting
+		if (outputPath && outputPath.includes("/feed.")) {
+			return content;
+		}
+
+		return await prettier.format(content, {
+			parser: "html",
+			...options,
+		});
+	};
 }
 
 function configurePrettier(eleventyConfig, options = {}) {
-  eleventyConfig.addTransform('prettier', createPrettierTransform(options));
+	eleventyConfig.addTransform("prettier", createPrettierTransform(options));
 }
 
-export {
-  createPrettierTransform,
-  configurePrettier
-};
+export { createPrettierTransform, configurePrettier };
