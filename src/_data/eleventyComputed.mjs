bdb81dcfff0b5836417c012e823cfb20c5902ab9
Stefan <git@stefn.co.uk>
2025-11-23 13:26:13 +0000

    Add item thumbnails

--- src/_data/eleventyComputed.mjs
+++ src/_data/eleventyComputed.mjs
@@ -2,8 +2,10 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
 export default {
-	header_text: (data) => data.header_text || data.title,
-	meta_title: (data) => data.meta_title || data.title,
-	description: (data) => data.snippet || data.meta_description || "",
-	contactForm: () => require("./contact-form.json"),
+  header_text: (data) => data.header_text || data.title,
+  meta_title: (data) => data.meta_title || data.title,
+  description: (data) => data.snippet || data.meta_description || "",
+  contactForm: () => require("./contact-form.json"),
+  thumbnail: (data) =>
+    data.thumbnail || (data.gallery && data.gallery[0]) || data.header_image,
 };
