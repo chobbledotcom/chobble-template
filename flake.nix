4edb64d4bbd9ef2d2b0bf1a3e2ca0b6e14028c0c
Stefan <git@stefn.co.uk>
2025-11-29 11:39:11 +0000

    Use pnpm

--- flake.nix
+++ flake.nix
@@ -14,18 +14,22 @@
         in
         {
           default = pkgs.mkShell {
-            packages = with pkgs; [ nodejs_24 biome ];
+            packages = with pkgs; [
+              nodejs_24
+              pnpm
+              biome
+            ];
 
             shellHook = ''
-              [ ! -d node_modules ] && npm install
+              [ ! -d node_modules ] && pnpm install
               cat <<EOF
 
               Development environment ready!
 
               Available commands:
-               - 'npm run serve'   # Clean & start dev server with incremental builds
-               - 'npm run build'   # Clean & build the site in ./_site
-               - 'npm test'        # Run JavaScript tests
+               - 'pnpm run serve'  # Clean & start dev server with incremental builds
+               - 'pnpm run build'  # Clean & build the site in ./_site
+               - 'pnpm test'       # Run JavaScript tests
                - 'lint'            # Format code with Biome (Nix-only)
                - 'screenshot'      # Take website screenshots (Nix-only)
 
